16c758122b80eeac6451d1bada7ad8751cb2ebc2
Joby John <joby432john@gmail.com>
2022-07-30 17:57:39 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numbers_functions_csharp
+{
+    class Class1
+    {
+        public void sum_calc()
+        {
+            Console.Write("Enter the first number of your choice to add : ");
+            int firstnum = Convert.ToInt32( Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            int secondnum = Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("Sum is " + (firstnum + secondnum));
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace numbers_functions_csharp
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(10 / 3);//just a  regular divivsion
+        Console.WriteLine(Math.Pow(10, 3));//gives 10 raise to 3
+        Console.WriteLine(Math.Abs(-100));//gives absolute value
+        Console.WriteLine(Math.Sqrt(49));//gives square root
+        Console.WriteLine(Math.Max(50, 50.5));//gives the greater and Min does the opposite
+        Console.WriteLine(Math.Round(50.9));//rounds the number
+        Console.Write("Enter a number: ");
+
+        string txt = Console.ReadLine();
+        Console.WriteLine("You entered: " + txt);
+        Class1 class1 = new Class1(); // assigning a variable to the class and defining it i think 
+        class1.sum_calc(); //calling the function
+
+    }
+}
+}
+
